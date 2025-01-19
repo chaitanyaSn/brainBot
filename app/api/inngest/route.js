ce7ef6552296616c723a1a040a0b9383f049b34d
ChaitanyaSn <chaitanyascode24@gmail.com>
2025-01-19 13:54:57 +0530

    api/inngest req delay

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,16 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { CreateNewUser, GenrateNotes, helloWorld } from "@/inngest/functions";
-export const runtime='edge'
+
+// Remove the edge runtime
+export const runtime = 'nodejs';
+// Set to maximum allowed for hobby plan
+export const maxDuration = 60;
+
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  streaming:'allow',
+  streaming: 'allow',
   functions: [
     helloWorld,
     CreateNewUser,
